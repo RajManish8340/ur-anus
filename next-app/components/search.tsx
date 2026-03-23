4a481b6f446365c6a995d81f041ec3afef74642c
Raj pop <rajmanishsahu1121@gmail.com>
2026-03-23 19:36:20 +0530

    some more ui changes

--- next-app/components/search.tsx
+++ next-app/components/search.tsx
@@ -50,7 +50,7 @@ export default function TokenSearch() {
 
 
   return (
-    <div className="flex items-center justify-center py-5">
+    <div className="flex flex-col items-center py-5">
       <div className="flex flex-col items-center justify-between m-2" >
 
         <input className="p-2 bg-red-300 font-black text-black rounded-2xl"
@@ -62,10 +62,10 @@ export default function TokenSearch() {
 
         {click &&
           <ul className="m-2 bg-amber-300 text-black rounded-2xl">
-            {filtered.slice(0, 15).map(t => (
+            {filtered.slice(0, 7).map(t => (
               <li className="p-1"
                 key={t.id}
-                onClick={() => setSelectedToken(t)}
+                onClick={() => { setSelectedToken(t); setClick(false) }}
                 style={{ cursor: "pointer" }}
               >
                 {t.symbol} - {t.name}
@@ -75,24 +75,26 @@ export default function TokenSearch() {
         }
       </div>
 
-      {selectedToken && (
-        <div className="bg-orange-300 text-black rounded-md p-2">
-          <h3>
-            {selectedToken.symbol} - {selectedToken.name}
-          </h3>
-          <p>Mint: {selectedToken.id}</p>
+      {
+        selectedToken && (
+          <div className="bg-orange-300 text-black rounded-md p-2">
+            <h3>
+              {selectedToken.symbol} - {selectedToken.name}
+            </h3>
+            <p className="wrap-anywhere">Mint: {selectedToken.id}</p>
 
-          {priceData ? (
-            <>
-              <p>Jupiter: {priceData.prices?.jup ?? "N/A"}</p>
-              <p>Orca: {priceData.prices?.orca ?? "N/A"}</p>
-              <p>Raydium: {priceData.prices?.radium ?? "N/A"}</p>
-            </>
-          ) : (
-            <p>Loading price...</p>
-          )}
-        </div>
-      )}
-    </div>
+            {priceData ? (
+              <>
+                <p>Jupiter: {priceData.prices?.jup ?? "N/A"}</p>
+                <p>Orca: {priceData.prices?.orca ?? "N/A"}</p>
+                <p>Raydium: {priceData.prices?.radium ?? "N/A"}</p>
+              </>
+            ) : (
+              <p>Loading price...</p>
+            )}
+          </div>
+        )
+      }
+    </div >
   );
 }
